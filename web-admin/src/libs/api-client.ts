4bae0ca630b7b9fa30ad61aba660cc2f78570d3c
elpmid <condinoaljoseph@gmail.com>
2025-09-06 22:08:40 +0800

    feat: add axios

---
+++ web-admin/src/libs/api-client.ts
@@ -0,0 +1,16 @@
+import Axios, { InternalAxiosRequestConfig } from 'axios';
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  if (config.headers) {
+    config.headers.Accept = 'application/json';
+  }
+
+  config.withCredentials = true;
+  return config;
+}
+
+export const api = Axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+api.interceptors.request.use(authRequestInterceptor);
